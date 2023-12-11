d57a9ff59537337fa104e3354f189f05ec4b6a0b
darkcodi <trooper982@gmail.com>
2023-12-11 08:11:32 -0600

    Revert "Add alternative solution for day01"
    This reverts commit ce02e7bebf225f30d4874dad51324d1ad3226ff9.

--- src/day01/main.rs
+++ src/day01/main.rs
@@ -51,35 +51,6 @@ fn part2(lines: &Vec<&str>) {
     println!("Part 2: {}", sum);
 }
 
-const NUMBERS: [&str; 9] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-fn part2_alt(lines: &Vec<&str>) {
-    let mut sum = 0;
-    for line in lines {
-        let mut first_number = None;
-        let mut last_number = None;
-        for c in line.chars() {
-            let number = c.to_digit(10).or_else(|| {
-                for (i, number) in NUMBERS.iter().enumerate() {
-                    if number.starts_with(c) {
-                        return Some((i + 1) as u32);
-                    }
-                }
-                None
-            });
-            if number.is_some() {
-                if first_number == None {
-                    first_number = number;
-                }
-                last_number = number;
-            }
-        }
-        if first_number.is_some() {
-            sum += first_number.unwrap() * 10 + last_number.unwrap();
-        }
-    }
-    println!("Part 2: {}", sum);
-}
-
 struct CycleBuffer {
     buffer: [char; 5],
     index: usize,
@@ -133,23 +104,16 @@ impl CycleBuffer {
 extern crate test;
 use test::Bencher;
 
-// #[bench]
-// fn test_part1(b: &mut Bencher) {
-//     let input = std::fs::read_to_string("src/day01/input.txt").unwrap();
-//     let lines: Vec<&str> = input.lines().collect();
-//     b.iter(|| part1(&lines));
-// }
+#[bench]
+fn test_part1(b: &mut Bencher) {
+    let input = std::fs::read_to_string("src/day01/input.txt").unwrap();
+    let lines: Vec<&str> = input.lines().collect();
+    b.iter(|| part1(&lines));
+}
 
 #[bench]
 fn test_part2(b: &mut Bencher) {
     let input = std::fs::read_to_string("src/day01/input.txt").unwrap();
     let lines: Vec<&str> = input.lines().collect();
     b.iter(|| part2(&lines));
-}
-
-#[bench]
-fn test_part2_alt(b: &mut Bencher) {
-    let input = std::fs::read_to_string("src/day01/input.txt").unwrap();
-    let lines: Vec<&str> = input.lines().collect();
-    b.iter(|| part2_alt(&lines));
 }
